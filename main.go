94159fd24863643bfceba7e0273196704a3fb39b
Kyle Phillips <kphillips375@yahoo.com>
2024-11-30 00:51:44 -0500

    server setup

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+import (
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+
+)
+
+
+func main() {
+e := echo.New()
+e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+	AllowOrigins:     []string{"http://www.localhost:3000/"},
+	AllowHeaders:     []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept},
+	AllowCredentials: true, // Set to true if you want to allow credentials (cookies, HTTP authentication) to be included in the CORS request
+	MaxAge:           3600, // Max age of the CORS options preflight request in seconds
+}))
+e.Logger.Fatal(e.Start(":8000"))
+
+}
