2e3ff41761c69765ad500feca535bc4fc07cd727
Kyle Phillips <kphillips375@yahoo.com>
2024-12-19 21:39:09 -0500

    goal routes

---
+++ routes/goal_routes.go
@@ -0,0 +1,11 @@
+package routes
+
+import (
+	"github.com/KPWithCode/statpad2/handlers"
+	"github.com/labstack/echo/v4"
+)
+
+func GoalRoutes(e *echo.Echo) {
+	// Define a route for processing goals
+	e.GET("/process-goals", handlers.ProcessGoalsHandler)
+}
