669f2823ff1a24573553a921bc389d1201953fd3
Kyle Phillips <kphillips375@yahoo.com>
2024-12-27 08:42:09 -0500

    updated route imports

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ import (
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	"github.com/KPWithCode/statpad2/routes"
+    nba "github.com/KPWithCode/statpad2/routes/nbaroutes"
 
 )
 
@@ -16,8 +17,12 @@ e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
 	MaxAge:           3600, // Max age of the CORS options preflight request in seconds
 }))
 
+	routes.EventRoutes(e)
 	routes.AssistRoutes(e)
 	routes.GoalRoutes(e)
+	nba.NBARoutes(e)
+	
+
 	
 e.Logger.Fatal(e.Start(":8000"))
 
