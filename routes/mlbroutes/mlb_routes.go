2b742f527e475a66ad03556735662210f5fc04c4
Kyle Phillips <kphillips375@yahoo.com>
2025-01-30 00:09:16 -0500

    named mlb routes

--- routes/mlbroutes/mlb_routes.go
+++ routes/mlbroutes/mlb_routes.go
@@ -1,11 +1,11 @@
-package nbaroutes
+package mlbroutes
 
 import (
 	"github.com/KPWithCode/statpad2/handlers/mlbhandler"
 	"github.com/labstack/echo/v4"
 )
 
-func NBARoutes(e *echo.Echo) {
+func MLBRoutes(e *echo.Echo) {
 	// Define a route for processing goals
 	e.GET("/mlb/pythagorean", mlbhandler.PythagoreanHandler)
 }
